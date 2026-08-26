a637ed452d9bb79445c63c728ddf6f08f5f5effb
Ayush mittal <163040899+ayushmittal9@users.noreply.github.com>
2026-08-26 23:13:26 +0530

    gkjb
    kjjknlknl kln

--- frontend/src/Components/Card/CardSection.jsx
+++ frontend/src/Components/Card/CardSection.jsx
@@ -100,7 +100,7 @@ function CardSection() {
     <div className="CardSection">
       <div className="para">
         <p>Check out our latest products!</p>
-        <p> sckdjscbkdsncjkdjk djkcdcd kjdskchlkdn </p>
+        <h1>cbkdjc djkkckd</h1>
       </div>
 
       <div className="slider-wrapper">
